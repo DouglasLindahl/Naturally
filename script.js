e1bd590001ca11c22498d5e30f201a7dfbcfcd1f
gustavenoksson <gustav.enoksson@live.se>
2022-11-16 14:32:27 +0100

    added for loop js

--- script.js
+++ script.js
@@ -3,10 +3,41 @@ const body = document.querySelector("body");
 const elementCardsRightArrow = document.querySelector(".rightArrow");
 const elementCardsLeftArrow = document.querySelector(".leftArrow");
 const elementCardNavigation = document.querySelector(".cardNavigation")
+const elementCardsContainer = document.querySelector(".elementCardsContainer");
+const elementCardsScrollContainer = document.querySelector(".scrollContainer");
 
 
 let currentElementCard = 0;
 const howManyCards = 4;
+
+const card = {header: "CONTROL THE ELEMENTS", img:"images/triangle.png", infoHeader: "ALWAYS WATERPROOF", info: "Shields you and keeps you dry in all your endeavours. From snowstorms to moonsoon rain, or while commuting to work."};
+
+for (let i = 0; i < 4; i++) {
+    let elementCard = document.createElement("article");
+    elementCard.classList.add("elementCard")
+
+    let cardHeader = document.createElement("h3");
+    let cardImg = document.createElement("img");
+
+    let cardText = document.createElement("div");
+    elementCard.classList.add("elementText")
+
+    let cardInfoHeader = document.createElement("h4");
+    let cardInfoText = document.createElement("p");
+
+    cardHeader.textContent = card.header;
+    cardImg.src = card.img;
+    cardInfoHeader.textContent = card.infoHeader;
+    cardInfoText.textContent = card.info;
+
+    cardText.append(cardInfoHeader);
+    cardText.append(cardInfoText);
+    elementCard.append(cardHeader);
+    elementCard.append(cardImg);
+    elementCard.append(cardText);
+
+    elementCardsScrollContainer.append(elementCard);
+}
 
 for(let i = 0; i <= howManyCards; i++)
 {
