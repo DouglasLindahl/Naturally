44120862c8910775ec714dba322ab73fea9eadf1
gustavenoksson <gustav.enoksson@live.se>
2022-11-29 13:07:21 +0100

    changes to collection js

--- collection.js
+++ collection.js
@@ -2,22 +2,48 @@ let collectionSection = document.querySelector(".collectionSection");
 let collectionCards = document.querySelector(".collectionCards");
 
 const collectionCardsInfo = [
-    {name: "jacket", description: "", price: 2495},
-    {name: "trousers", description: "", price: 2495},
-    {name: "cap", description: "", price: 2495},
+    {name: "jacket", description: "THE XTREME JACKET 3.0", price: 2495},
+    {name: "trousers", description: "THE XTREME TROUSERS 3.0", price: 2495},
+    {name: "cap", description: "THE XTREME CAP 3.0", price: 2495},
 ]
 
 const variants = [
-    {name: "jacket", normalUrl: "", greenUrl: ""},
+    {name: "jacket", normalUrl: "images/collection/Jacket-green-180.mp4", greenUrl: ""},
     {name: "trousers", normalUrl: "", greenUrl: ""},
     {name: "cap", normalUrl: "", greenUrl: ""},
 ]
 
 for(let i = 0; i < collectionCardsInfo.length; i++)
 {
+    // let collectionCard = document.createElement("article");
+    // let collectionCardImageContainer = document.createElement("div");
+    // let collectionCardImage = document.createElement("img");
+    // let collectionCardText = document.createElement("div");
+    // let itemDescription = document.createElement("p");
+    // let itemPrice = document.createElement("p");
+
+    // collectionCard.classList.add("collectionCard");
+    // collectionCardImageContainer.classList.add("collectionCardImage");
+    // collectionCardText.classList.add("collectionCardText");
+    // itemDescription.classList.add("itemDescription");
+    // itemPrice.classList.add("itemPrice");
+
+    // collectionCard.append(collectionCardImageContainer);
+    // collectionCardImageContainer.append(collectionCardImage);
+    // collectionCard.append(collectionCardText);
+    // collectionCardText.append(itemDescription);
+    // collectionCardText.append(itemPrice);
+
+    // collectionCardImage.src = variants[i].normalUrl;
+    // itemDescription.textContent = collectionCardsInfo[i].description;
+    // itemPrice.textContent = collectionCardsInfo[i].price;
+    
+    // collectionCards.append(collectionCard);
+
     let collectionCard = document.createElement("article");
     let collectionCardImageContainer = document.createElement("div");
-    let collectionCardImage = document.createElement("img");
+    let collectionCardVideo = document.createElement("video");
+    let collectionCardVideoSrc = document.createElement("source");
     let collectionCardText = document.createElement("div");
     let itemDescription = document.createElement("p");
     let itemPrice = document.createElement("p");
@@ -29,12 +55,13 @@ for(let i = 0; i < collectionCardsInfo.length; i++)
     itemPrice.classList.add("itemPrice");
 
     collectionCard.append(collectionCardImageContainer);
-    collectionCardImageContainer.append(collectionCardImage);
+    collectionCardImageContainer.append(collectionCardVideo);
     collectionCard.append(collectionCardText);
     collectionCardText.append(itemDescription);
     collectionCardText.append(itemPrice);
+    collectionCardVideo.append(collectionCardVideoSrc);
 
-    collectionCardImage.src = variants[i].normalUrl;
+    collectionCardVideoSrc.src = variants[i].normalUrl;
     itemDescription.textContent = collectionCardsInfo[i].description;
     itemPrice.textContent = collectionCardsInfo[i].price;
     
