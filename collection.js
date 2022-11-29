627c647eae7ba987ad23746cda8246a50c78a159
DouglasLindahl <douglas.lindahl@gmail.com>
2022-11-29 12:25:32 +0100

    asd

--- collection.js
+++ collection.js
@@ -1,9 +1,42 @@
-const collectionSection = document.querySelector(".collectionSection");
-const collectionCardsContainer = document.querySelector(".collectionCards");
+let collectionSection = document.querySelector(".collectionSection");
+let collectionCards = document.querySelector(".collectionCards");
 
-const collectionCards = [
-    {img:"images/collection/jacket-yellow.gif", description:"THE XTREME JACKET 3.0", price:"2495 SEK"},
-    {img:"images/collection/yellow-pants-180 (1).gif", description:"THE XTREME TROUSERS 3.0", price:"2495 SEK"},
-    {img:"images/collection/cap-box.png", description:"THE XTREME HAT 3.0", price:"495 SEK"},
-    {img:"images/collection/keychain.png", description:"THE XTREME KEYRING 3.0", price:"495 SEK"}
+const collectionCardsInfo = [
+    {name: "jacket", description: "", price: 2495},
+    {name: "trousers", description: "", price: 2495},
+    {name: "cap", description: "", price: 2495},
 ]
+
+const variants = [
+    {name: "jacket", normalUrl: "", greenUrl: ""},
+    {name: "trousers", normalUrl: "", greenUrl: ""},
+    {name: "cap", normalUrl: "", greenUrl: ""},
+]
+
+for(let i = 0; i < collectionCardsInfo.length; i++)
+{
+    let collectionCard = document.createElement("article");
+    let collectionCardImageContainer = document.createElement("div");
+    let collectionCardImage = document.createElement("img");
+    let collectionCardText = document.createElement("div");
+    let itemDescription = document.createElement("p");
+    let itemPrice = document.createElement("p");
+
+    collectionCard.classList.add("collectionCard");
+    collectionCardImageContainer.classList.add("collectionCardImage");
+    collectionCardText.classList.add("collectionCardText");
+    itemDescription.classList.add("itemDescription");
+    itemPrice.classList.add("itemPrice");
+
+    collectionCard.append(collectionCardImageContainer);
+    collectionCardImageContainer.append(collectionCardImage);
+    collectionCard.append(collectionCardText);
+    collectionCardText.append(itemDescription);
+    collectionCardText.append(itemPrice);
+
+    collectionCardImage.src = variants[i].normalUrl;
+    itemDescription.textContent = collectionCardsInfo[i].description;
+    itemPrice.textContent = collectionCardsInfo[i].price;
+    
+    collectionCards.append(collectionCard);
+}
