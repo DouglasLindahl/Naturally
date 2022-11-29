d082e2ac655ea260099a67e0f68dc9631b6664b5
gustavenoksson <gustav.enoksson@live.se>
2022-11-29 09:28:18 +0100

    added collection images

---
+++ collection.js
@@ -0,0 +1,9 @@
+const collectionSection = document.querySelector(".collectionSection");
+const collectionCardsContainer = document.querySelector(".collectionCards");
+
+const collectionCards = [
+    {img:"images/collection/jacket-yellow.gif", description:"THE XTREME JACKET 3.0", price:"2495 SEK"},
+    {img:"images/collection/yellow-pants-180 (1).gif", description:"THE XTREME TROUSERS 3.0", price:"2495 SEK"},
+    {img:"images/collection/cap-box.png", description:"THE XTREME HAT 3.0", price:"495 SEK"},
+    {img:"images/collection/keychain.png", description:"THE XTREME KEYRING 3.0", price:"495 SEK"}
+]
