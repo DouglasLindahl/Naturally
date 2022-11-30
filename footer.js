b5b050de66cb3733d286ca8a6c9abb877edc1c47
DouglasLindahl <douglas.lindahl@gmail.com>
2022-11-30 15:13:21 +0100

    footer buttonimages

---
+++ footer.js
@@ -0,0 +1,18 @@
+
+let socialMedia = document.querySelector(".socialMedia");
+let payment = document.querySelector(".payment");
+
+let klarna = document.querySelector(".klarna");
+let mastercard = document.querySelector(".mastercard");
+let paypal = document.querySelector(".paypal");
+
+const socialMediaImages = [
+    twitter = document.querySelector(".twitter"),
+    facebook = document.querySelector(".facebook"),
+    instagram = document.querySelector(".instagram"),
+    youtube = document.querySelector(".youtube")
+]
+
+socialMediaImages.forEach(image => {
+    
+});
